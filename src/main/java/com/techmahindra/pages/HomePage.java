fadb47648e38d90d9249a9f328bc448bc40b5339
SachinKamat <2000.kamath@gmail.com>
2017-07-24 17:48:36 +0530

    Updated HomePage.java

--- src/main/java/com/techmahindra/pages/HomePage.java
+++ src/main/java/com/techmahindra/pages/HomePage.java
@@ -9,4 +9,24 @@ import net.thucydides.core.annotations.DefaultUrl;
 @DefaultUrl("https://stable.cortellis.int.clarivate.com")
 public class HomePage extends PageObject{
 
+    @FindBy(css = "a.primarynav__home")
+    WebElementFacade buttonHome;
+
+    @FindBy(xpath = ".//span[1][@class='navText' and contains(text(),'Home')]")
+    WebElementFacade homePage;
+
+    public void clickButtonHome() throws InterruptedException {
+        {
+            buttonHome.click();
+            // WebDriverUtils.waitForElementLoading(5);
+            Thread.sleep(3000);
+        }
+    }
+
+    public boolean isHomePageDisplayed() {
+        if (!homePage.isDisplayed()) {
+            return true;
+        }
+        else return false;
+    }
 }
